2acf38a9240f372823b024bbe6ab6f34da8a7a4b
israelxgbedey <88339733+israelxgbedey@users.noreply.github.com>
2023-06-11 03:33:03 +0000

    Add files via upload

---
+++ Shippingapptest/precache-manifest.e99a9ecec52146a4100b783e99c9d9fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c019c8492bb505a898d3",
+    "url": "./static/css/main.a4d73efb.chunk.css"
+  },
+  {
+    "revision": "c019c8492bb505a898d3",
+    "url": "./static/js/main.c019c849.chunk.js"
+  },
+  {
+    "revision": "ed197c0f677114f68a6f",
+    "url": "./static/js/1.ed197c0f.chunk.js"
+  },
+  {
+    "revision": "aef68a3844a3836bc8cd",
+    "url": "./static/js/2.aef68a38.chunk.js"
+  },
+  {
+    "revision": "a178e178947684996785",
+    "url": "./static/js/runtime~main.a178e178.js"
+  },
+  {
+    "revision": "699d816c8b395d138a8ffbf2c2b38dd7",
+    "url": "./index.html"
+  }
+];
